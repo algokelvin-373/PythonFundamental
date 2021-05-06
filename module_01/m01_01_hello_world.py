9a5110dc145018250585284327b15be87505cdc0
kelvin373-ht <kelvin373.ht@gmail.com>
2021-05-06 15:31:52 +0700

    Repair program again

--- module_01/m01_01_hello_world.py
+++ module_01/m01_01_hello_world.py
@@ -1,3 +1,4 @@
 print("Hello World")
-print("My name is Algokelvin. I like learn Python Programming")
+print("My name is Algokelvin. I like learn "
+      "Python Programming")  # Print with line
 print("This is first time learn Python")

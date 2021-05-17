e6c463dc595381ef2034aee8049b3319e197b260
kelvin373-ht <kelvin373.ht@gmail.com>
2021-05-17 21:40:27 +0700

    M022: Ternary Tuple

--- module_fundamental/m021_ternary_operator.py
+++ module_fundamental/m021_ternary_operator.py
@@ -1,3 +1,4 @@
 print("***** Operator Tenary *****")
 bil1 = int(input("Input number = "))
-print(bil1, "is ", "negative" if (bil1 < 0) else "positive")
+status = "negative" if (bil1 < 0) else "positive"
+print(bil1, "is ", status)
